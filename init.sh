90364d47bdf5147a61a69306d37327b65deaad4c
Konstantin Alekseev <kv.alekseev@gmail.com>
2011-05-04 10:47:11 +0400

    my.zsh

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ ln -s dotfiles/.gitignore .gitignore
 ln -s dotfiles/.gitconfig .gitconfig
 ln -s dotfiles/.gemrc .gemrc
 echo 'source ~/dotfiles/.bashrc' >> .bashrc
+ln -s ~/dotfiles/my.zsh ~/.oh-my-zsh/custom/my.zsh
