6dce7ff8e25b843c3f855def59cbf0de1f9517c4
Konstantin Alekseev <kv.alekseev@gmail.com>
2011-06-24 08:39:10 +0400

    aliases

--- my.zsh
+++ my.zsh
@@ -11,32 +11,7 @@ export HISTCONTROL=erasedups
 export HISTTIMEFORMAT="%d-%m-%y %T "
 
 #aliases
-alias gs='git status '
-alias ga='git add '
-alias gb='git branch '
-alias gc='git commit'
-alias gd='git diff'
-alias go='git checkout '
-alias gk='gitk --all&'
-alias gx='gitx --all'
-
-alias got='git '
-alias get='git '
-
-alias sd='sudo'
-alias vi='vim'
-alias ac='apt-cache'
-alias acs='apt-cache search'
-
-alias rds='rdesktop -k en-us -g 1200x900 -u Administrator $@ > /dev/null 2>&1'
-#alias gw_rd='ssh -f gw.loc -L 3389:192.168.22.101:3389 -N'
-#alias gw_sql='ssh -f gw.loc -L 1433:192.168.22.101:1433 -N'
-#alias gw_tr='ssh -f gw.loc -L 9091:localhost:9091 -N'
-
-alias rsqlplus='rlwrap sqlplus'
-alias rrman='rlwrap rman'
-
-alias sap='~/git/oradmin/sap.rb'
+source $HOME/dotfiles/aliases
 
 bindkey '5D' backward-word
 bindkey '5C' forward-word
