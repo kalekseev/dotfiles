fd0a94bdb7662fd2fecc755f3f93ca60d63ef8ad
Konstantin Alekseev <kv.alekseev@gmail.com>
2011-07-10 22:55:00 +0400

    qfix

--- init.sh
+++ init.sh
@@ -1,12 +1,17 @@
 cd ~
 rm -rf .gitignore
 ln -s dotfiles/gitignore .gitignore
+
 rm -rf .gitconfig
 ln -s dotfiles/gitconfig .gitconfig
+
 rm -rf .gemrc
 ln -s dotfiles/gemrc .gemrc
+
 rm -rf .zshrc
 ln -s dotfiles/zsh/zshrc .zshrc
-echo 'source ~/dotfiles/bashrc' >> .bashrc
+
 ln -s ~/dotfiles/zsh/my.zsh ~/.oh-my-zsh/custom/my.zsh
-ln -s ~/dotfiles/zsh/plugins ~/.oh-my-zsh/custom/plugins
+ln -s ~/dotfiles/zsh/plugins ~/.oh-my-zsh/custom/
+
+echo 'source ~/dotfiles/bashrc' >> .bashrc
