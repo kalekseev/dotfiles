1e5a99c021c053b72f9cdaa05a10e8491504e90c
Konstantin Alekseev <kv.alekseev@gmail.com>
2011-08-05 11:14:47 +0400

    update post.rb

--- bin/post.rb
+++ bin/post.rb
@@ -1,54 +1,57 @@
 #!/usr/bin/env ruby
 #encoding: utf-8
+
 require "uri"
 require "net/http"
-require "nokogiri" if Gem::Specification::find_by_name('nokogiri')
+require "nokogiri"
 
-url_open_retry_times = 3
-
-
-#usps
+URL_OPEN_RETRY_TIMES = 3
 file_with_tracking_numbers = File.expand_path(File.join(File.dirname(__FILE__), '../usps.log'))
-usurl = 'http://trkcnfrm1.smi.usps.com/PTSInternetWeb/InterLabelInquiry.do'
-usparams = {'origTrackNum' => 'XXXXXXXXXXXXX', 'Go to Label/Receipt Number page' => 'Go'}
 
-
-unless File.readable?(file_with_tracking_numbers)
-  puts "File \"#{file_with_tracking_numbers}\" not exist or not readable" 
-  exit
-end
-
-array = IO.readlines(file_with_tracking_numbers)
-
-array.each do |id|
-  id.chomp!
-  break if id !~ /.*US/
-  usparams['origTrackNum']=id
+def get_html_body(url,params)
+  i = URL_OPEN_RETRY_TIMES 
   begin
-    html = Net::HTTP.post_form(URI.parse(usurl), usparams)
+    html = Net::HTTP.post_form(URI.parse(url), params)
   rescue Exception => e
-    retry if ((url_open_retry_times -= 1 ) > 0 && e.message == "Timeout::Error")
+    retry if ((i -= 1 ) > 0 && e.message == "Timeout::Error")
     puts e.message
-    next
+    [1,0]
   end
-  puts "\033[31mUSPS: #{id}\n--------------------\033[0m"
+
   if html.code != '200'
     puts "HTTP response not OK"
-    next
+    [1,0]
   end
-  found = false
-  html.body.each_line do |line|
-    if (line =~ /^\s+(.*) Information, if available, is updated periodically throughout the day. Please check again later./)
-      puts "\033[32m#{$1}\033[0m\n\n"
-      found = true
-      break
-    end
-  end
-  puts "Item not found\n\n" unless found
+
+  [0, html.body]
 end
 
-#russian post
-if Gem::Specification::find_by_name('nokogiri')
+def usps(array)
+
+  usurl = 'http://trkcnfrm1.smi.usps.com/PTSInternetWeb/InterLabelInquiry.do'
+  usparams = {'origTrackNum' => 'XXXXXXXXXXXXX', 'Go to Label/Receipt Number page' => 'Go'}
+
+  array.each do |id|
+    id.chomp!
+    break if id !~ /.*US/
+    usparams['origTrackNum']=id
+    puts "\033[31mUSPS: #{id}\n--------------------\033[0m"
+
+    (exit_code, body) = get_html_body(usurl,usparams)
+    next unless exit_code == 0
+    doc = Nokogiri.HTML(body, nil, 'UTF-8')
+    td = doc.xpath("/html/body/table[4]/tr/td[2]/table/tr[3]/td/table/tr/td[2]/table/tr[4]/td[2]/table/tr")
+    if td.empty?
+      puts "Item not found\n\n"
+      next
+    end
+    (1...td.count).each {|t| puts "\033[4;33m*\033[0m #{td[t].inner_text.lstrip.rstrip}"}
+    puts
+
+  end
+end
+
+def rupost(array)
 
   ruurl = 'http://www.russianpost.ru/rp/servise/ru/home/postuslug/trackingpo'
   ruparams = {'BarCode' => 'XXXXXXXXXXXXX', 'searchsign' => '1'}
@@ -56,24 +59,18 @@ if Gem::Specification::find_by_name('nokogiri')
   array.each do |id|
     id.chomp!
     ruparams['BarCode'] = id
-    begin
-      html = Net::HTTP.post_form(URI.parse(ruurl), ruparams)
-    rescue Exception => e
-      retry if (url_open_retry_times -= 1 ) > 0
-      puts e.message
-      next
-    end
     puts "\033[31mRUPOST: #{id}\n----------------------\033[0m"
-    if html.code != '200'
-      puts "HTTP response not OK"
-      next
-    end
-    doc = Nokogiri.HTML(html.body, nil, 'UTF-8')
+
+    (exit_code, body) = get_html_body(ruurl,ruparams)
+    next unless exit_code == 0
+
+    doc = Nokogiri.HTML(body, nil, 'UTF-8')
     td = doc.xpath("/html/body/form/table/tr/td[2]/div/div/table[4]/tbody/tr/td")
     if td.empty?
       puts "Item not found\n\n"
       next
     end
+
     just = [16,16,6,36,36,6,5,5,6,24]
     just.map! {|item| item + 1}
     names = ["status", "date", "index", "name", "attribute", 
@@ -89,6 +86,14 @@ if Gem::Specification::find_by_name('nokogiri')
     end
     puts
   end
-else
-  puts "Nokogiri gem not available, install it for russia post traking"
 end
+
+unless File.readable?(file_with_tracking_numbers)
+  puts "File with tracking numbers \"#{file_with_tracking_numbers}\" not exist or not readable"
+  exit
+end
+
+array = IO.readlines(file_with_tracking_numbers)
+
+usps(array)
+rupost(array)
