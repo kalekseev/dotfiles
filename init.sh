e7d9a6f0459b82669e8504844533f7dc2b6b75fc
Konstantin Alekseev <kv.alekseev@gmail.com>
2011-10-14 11:16:39 +0400

    add octaverc

--- init.sh
+++ init.sh
@@ -11,6 +11,9 @@ ln -s dotfiles/gemrc .gemrc
 rm -rf .zshrc
 ln -s dotfiles/zsh/zshrc .zshrc
 
+rm -rf .octaverc
+ln -s dotfiles/octaverc .octaverc
+
 ln -s ~/dotfiles/zsh/my.zsh ~/.oh-my-zsh/custom/my.zsh
 ln -s ~/dotfiles/zsh/plugins ~/.oh-my-zsh/custom/
 
