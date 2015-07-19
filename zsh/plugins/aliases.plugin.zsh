4d22f58e79f2b42a3b33394633a8535d41d10e01
Konstantin Alekseev <kv.alekseev@gmail.com>
2015-07-19 20:07:39 +0300

    Add explain func

--- zsh/plugins/aliases.plugin.zsh
+++ zsh/plugins/aliases.plugin.zsh
@@ -35,3 +35,18 @@ set_hg_prompt() {
 mkcd() {
     mkdir -p "$1" && cd "$1"
 }
+
+explain () {
+  if [ "$#" -eq 0 ]; then
+    while read  -p "Command: " cmd; do
+      curl -Gs "https://www.mankier.com/api/explain/?cols="$(tput cols) --data-urlencode "q=$cmd"
+    done
+    echo "Bye!"
+  elif [ "$#" -eq 1 ]; then
+    curl -Gs "https://www.mankier.com/api/explain/?cols="$(tput cols) --data-urlencode "q=$1"
+  else
+    echo "Usage"
+    echo "explain                  interactive mode."
+    echo "explain 'cmd -o | ...'   one quoted command to explain it."
+  fi
+}
