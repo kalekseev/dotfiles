4861e780475e6b0e103fde24b19f0cd9cc6b3e0b
Konstantin Alekseev <kv.alekseev@gmail.com>
2018-05-26 17:06:57 +0300

    Update

--- Makefile
+++ Makefile
@@ -3,16 +3,10 @@ PHONY: all link theme package font
 package:
 	~/dotfiles/install/package.sh
 
-theme:
-	~/dotfiles/install/theme.sh
-
 link:
 	~/dotfiles/install/link.sh
 
 font:
 	~/dotfiles/install/font.sh
 
-update:
-	~/dotfiles/install/update.sh
-
-all: package link font theme update
+all: package link font
--- install/package.sh
+++ install/package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Install packages"
-packages=("wmctrl"  "tmux"  "xclip"  "rxvt-unicode"  "git"  "zsh")
+packages=("tmux"  "xclip"  "git"  "zsh" "mercurial")
 
 if dpkg -l "${packages[@]}" | grep "^ii" > /dev/null 2>&1; then
-    sudo apt-get install --yes "${packages[@]}"
+    sudo apt install --yes "${packages[@]}"
 else
     echo "Everything installed"
 fi
