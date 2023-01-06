9723d57dab21773b36e34327dd4328eb3a30992d
Konstantin Alekseev <mail@kalekseev.com>
2023-01-06 22:09:09 +0200

    Add RunSQL injection for python

---
+++ vim/after/queries/python/injections.scm
@@ -0,0 +1,10 @@
+; extends
+
+(call
+  (attribute
+    object: (identifier) @_path (#eq? @_path "migrations")
+    attribute: (identifier) @_name (#eq? @_name "RunSQL"))
+  (argument_list
+    (string) @sql
+    )
+  )
